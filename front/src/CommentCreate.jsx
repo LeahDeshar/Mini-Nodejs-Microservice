f592bdf2876c6e17cedb682d04b5f1778921193b
Leah Deshar <leah.desar@gmail.com>
2024-06-02 15:00:10 +0545

    comment listing

--- front/src/CommentCreate.jsx
+++ front/src/CommentCreate.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
-function CommentCreate() {
-  return <div>CommentCreate</div>;
+function CommentCreate({ postId }) {
+  const [content, setContent] = useState("");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+    setContent("");
+  };
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="form-group">
+          <label htmlFor="">New Comment</label>
+          <input
+            type="text"
+            className="form-control"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default CommentCreate;
