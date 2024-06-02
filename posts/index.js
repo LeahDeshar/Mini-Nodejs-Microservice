e2de933c151d0b9802e74cf9aab856997befa0f3
Leah Deshar <leah.desar@gmail.com>
2024-06-02 16:23:06 +0545

    event-bus implementation

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const app = express();
+const axios = require("axios");
 const cors = require("cors");
 app.use(bodyParser.json());
 app.use(cors());
@@ -11,7 +12,7 @@ const posts = {};
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
@@ -19,10 +20,20 @@ app.post("/posts", (req, res) => {
     id,
     title,
   };
-
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
   res.status(201).send(posts[id]);
 });
 
+app.post("/events", (req, res) => {
+  console.log("Received event", req.body.type);
+  res.send({});
+});
 app.listen(4000, () => {
   console.log("Listening on 4000");
 });
