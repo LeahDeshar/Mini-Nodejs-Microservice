6cfdbbbcc0e70b0a86c1735bba44d4e16962b334
Leah Deshar <leah.desar@gmail.com>
2024-06-02 14:44:13 +0545

    post listing

--- front/src/PostList.jsx
+++ front/src/PostList.jsx
@@ -1,7 +1,34 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function PostList() {
-  return <div>PostList</div>;
+  const [post, setpost] = useState({});
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await axios.get("http://localhost:4000/posts");
+      setpost(res.data);
+    };
+    fetchPost();
+  }, []);
+
+  const renderedPosts = Object.values(post).map((post) => {
+    return (
+      <div
+        key={post?.id}
+        className="card"
+        style={{ width: "30%", marginBottom: "20px" }}
+      >
+        <div className="card-body">
+          <h3>{post?.title}</h3>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
 }
 
 export default PostList;
