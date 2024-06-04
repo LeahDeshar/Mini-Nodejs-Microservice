0c04f5339120215bce0656f25fd8b75713fb01f1
Leah Deshar <leah.desar@gmail.com>
2024-06-04 17:51:31 +0545

    moderation service implementation

--- comments/index.js
+++ comments/index.js
@@ -34,6 +34,25 @@ app.post("/posts/:id/comments", async (req, res) => {
 });
 app.post("/events", (req, res) => {
   console.log("Received event", req.body.type);
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { postId, id, status } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
   res.send({});
 });
 app.listen(4001, () => {
