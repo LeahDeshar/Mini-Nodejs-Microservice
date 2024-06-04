9491011d71f02dd529750967ee29dd871a843e9c
Leah Deshar <leah.desar@gmail.com>
2024-06-04 17:42:17 +0545

    moderation service implementation

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,23 @@ const app = express();
 const axios = require("axios");
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        content: data.content,
+        postId: data.postId,
+        status,
+      },
+    });
+  }
+});
 
 app.listen(4003, () => {
   console.log("Listening on 4003");
